f75e04c995a0207362f7f4849b7d14152b2c2649
Pawel Chojnacki <pawel@chojnacki.ws>
2015-01-10 13:43:38 +0100

    x

--- 4th/a1.py
+++ 4th/a1.py
@@ -6,7 +6,7 @@ from numpy import *
 from scipy import *
 
 w = 50
-T = 2
+T = 1
 
 n = T * w
 t = linspace(0, T, n, endpoint=False)
@@ -16,12 +16,17 @@ signal = f(t)                 # funkcja sprÃ³bkowana
 
 subplot(211)
 plot(t, signal, '*')
+ylabel("amplituda")
+xlabel("t")
 signal1 = fft(signal)
-signal1 = abs(signal1)        # moduÅ‚
-
+signal1 = signal1
+print signal1
 subplot(212)
-freqs = range(n)              # <-- ZACZNIJ TUTAJ. UÅ¼yj linspace
-stem(freqs, signal1, '-*')
-xticks(arange(0, w, 1.0))
+freqs = linspace(0, w, len(signal1), endpoint=False)
+
+stem(freqs, signal1*2/n, '-*')
+xticks(arange(0, w+1, 5.0))
+xlabel("t")
+ylabel("Amplituda")
 
 show()
